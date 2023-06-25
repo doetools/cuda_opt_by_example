9634f730beb1fcd9da5a9408056a9615c9625bb9
SESA461392 <Wei.Tian@se.com>
2023-06-24 23:23:46 -0400

    add some utility functions

--- image_process/sobel_filter/sobel.cu
+++ image_process/sobel_filter/sobel.cu
@@ -1,9 +1,10 @@
 #include "../../cuda_opt_basics/data_structure.h"
-#include <stdint.h>
-
-#define STB_IMAGE_IMPLEMENTATION
 #include "../include/stb_image.h"
 #include "../include/stb_image_write.h"
+#include <stdint.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
 
 template <typename T> class Image : public DataBuffer<T> {
 
@@ -12,18 +13,21 @@ public:
   const int height;
   const int channel;
 
-  vector<T> image_data = vector<T>(this->size, 0);
+  vector<T> image = vector<T>(this->size, 0);
 
   Image(int width, int height, int channel, T *image)
       : DataBuffer<T>(width * height * channel, 1, false, false), width(width),
         height(height), channel(channel) {
-    // do something
+    convert_image_data(image);
   }
 
   int convert_image_data(T *image) {
-    for (int i = 0; i < height; i++) {
-      for (int j = 0; j < width; j++) {
-        for (int k = 0; k < CHANNEL_NUM; k++) {
+    int id = 0;
+    for (int k = 0; k < channel; k++) {
+      for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+          this->c_data[id] = image[i * width * channel + j * channel + k];
+          id += 1;
         }
       }
     }
@@ -31,7 +35,28 @@ public:
     return 0;
   }
 
-  int create_image_data() { return 0; }
+  int create_image_data() {
+    int id = 0;
+    for (int k = 0; k < channel; k++) {
+      for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+          image[i * width * channel + j * channel + k] = this->c_data[id];
+          id += 1;
+        }
+      }
+    }
+
+    return 0;
+  }
+
+  int save(char *file_name = "image.png") {
+    create_image_data();
+
+    stbi_write_png(file_name, width, height, channel, image.data(),
+                   width * channel);
+
+    return 0;
+  }
 };
 
 int main() {
@@ -43,44 +68,7 @@ int main() {
 
   stbi_image_free(rgb_image);
 
+  image.save();
+
   return 0;
 }
-
-int read(char *file_name = "original.png") {}
-
-// int save()
-// {
-//     int width = 800;
-//     int height = 800;
-//     int CHANNEL_NUM = 3;
-
-//     vector<uint8_t> rgb_image(width * height * CHANNEL_NUM, 255);
-
-//     int index = 0;
-
-//     for (int i = 0; i < height; i++)
-//     {
-//         for (int j = 0; j < width; j++)
-//         {
-//             for (int k = 0; k < CHANNEL_NUM; k++)
-//             {
-//                 if (k == 3)
-//                     rgb_image[i * width * CHANNEL_NUM + j * CHANNEL_NUM + k]
-//                     = 255;
-
-//                 if (k == 0)
-//                     rgb_image[i * width * CHANNEL_NUM + j * CHANNEL_NUM + k]
-//                     = 100;
-
-//                 if (k == 1)
-//                     rgb_image[i * width * CHANNEL_NUM + j * CHANNEL_NUM + k]
-//                     = uint8_t(rand() / 255);
-//             }
-//         }
-//     }
-
-//     stbi_write_png("image.png", width, height, CHANNEL_NUM, rgb_image.data(),
-//     width * CHANNEL_NUM);
-
-//     return 0;
-// }
